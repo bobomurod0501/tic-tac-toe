bb0abe1bb516c2faeb6c4ac469e96c8acdd64b37
bobomurod0501 <bobomurodeshmamatov@gmail.com>
2024-10-08 21:12:41 +0500

    loaded

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+
+const App = () => {
+  return (
+    <div className='container'>
+     <div>
+      <div className='box'></div>
+      <div className='box'></div>
+      <div className='box'></div>
+     </div >
+    </div>
+  )
+}
+
+export default App
